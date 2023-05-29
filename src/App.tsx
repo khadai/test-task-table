ca3bf4a5080c87a64912ed0079b0cfbeadb31d5d
khadai <mykhailo.khadai@gmail.com>
2023-05-29 21:31:32 +0300

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import './styles/App.scss';
 const App = () => {
     return (
         <div className="App">
-            <Table />
+            <div className="logo-container">
+                <div className="logo">
+                    <h3>LO</h3>
+                    <h3>GO</h3>
+                </div>
+            </div>
+            <div className="container">
+                <Table />
+            </div>
         </div>
     );
 };
